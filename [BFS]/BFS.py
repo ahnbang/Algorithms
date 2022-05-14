2c7976b38761e5810dfbe3c7bcd514c776a8bee3
Ahn Byoung Joon <97154684+ahnbang@users.noreply.github.com>
2022-05-14 19:06:52 +0900

    Update BFS.py

--- [BFS]/BFS.py
+++ [BFS]/BFS.py
@@ -1,68 +1,20 @@
-# Implement Queue class directly
-class Queue():
-    
-    def __init__(self):
-        
-        self.queue = []
-        
-    def Enqueue(self, data):
-        
-        self.queue.append(data)
-        
-    def Dequeue(self):
-        
-        Dequeue_Data = None
-        
-        if self.IsEmpty():
-            print("Queue is empty")
-        
-        else:
-            Dequeue_Data = self.queue[0]
-            self.queue = self.queue[1:]
-            
-        return Dequeue_Data
-    
-    def Peek(self):
-        
-        Peek_Data = None
-        
-        if self.IsEmpty():
-            print("Queue is empty")
-        
-        else:
-            Peek_Data = self.queue[0]
-            
-        return Peek_Data
-    
-    def IsEmpty(self):
-        
-        if len(self.queue) == 0:
-            
-            return True
-        else :  return False
+from collections import deque
+Graph = [[],[2,3,8],[1,7],[1,4,5],[3,5],[3,4],[7],[2,6,8],[1,7]] # link information
 
-def BFS1(Graph,Start_Vertex):
+def BFS(Graph, Start):
     
-    CV = Start_Vertex
-    stack = Queue()
+    queue = deque()
+    queue.append(Start)
     history = [False]*len(Graph)
-    stack.Enqueue(CV)
-    history[CV] = True
-    
-    
-    while stack.IsEmpty() != True :
-        cv = stack.Dequeue()
-        print(cv, end = ' ')
-        for vertex in Graph[cv]:
-            
-            if history[vertex] == True: pass
+    history[Start] = True
+    while queue:
         
+        vertex = queue.popleft()
+        print(vertex, end= '')
+        
+        for v in Graph[vertex]:
+            if history[v] == True: pass
             else:
-                 
-                 stack.Enqueue(vertex)
-                 history[vertex] = True
-    
-if __name__ == "__main__":
-    
-    Graph = [[],[2,3,8],[1,7],[1,4,5],[3,5],[3,4],[7],[2,6,8],[1,7]] # link information
-    BFS1(Graph,1)
+                queue.append(v)
+                history[v] = True
+BFS(Graph, 1)
